356dbe9484f29c08fc7cc4c26809485856128111
Quang Minh Pham <phamquangminhca@Quangs-MacBook-Pro.local>
2022-05-11 21:46:51 -0500

    Added 'tasks' and 'task-item' components

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
 import {Task} from '../../Task';
-import {TASKS} from '../../mock-tasks';
 
 
 @Component({
@@ -10,11 +10,12 @@ import {TASKS} from '../../mock-tasks';
 })
 export class TasksComponent implements OnInit {
 
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
 
 }
